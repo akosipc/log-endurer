0213e5138be359641685a24f36979f89d00ce01a
Adrian Peterson Co <adrianpetersonco@gmail.com>
2022-08-23 00:30:43 +0800

    Continues this tomorrow since I'm dead tired

--- lib/commander.rb
+++ lib/commander.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Commander
   class InvalidFilePathError < StandardError; end
 
@@ -6,5 +8,29 @@ class Commander
     @options = options
   rescue Errno::ENOENT
     raise InvalidFilePathError, "File is not existing. File path passed was #{File.join(__dir__, file_path)}"
+  end
+
+  def unique_views
+    x  = accumulate.then do |views|
+      grouped_views = views.group_by { |acc| acc.uri.value }
+
+      grouped_views.map do |key, value|
+        grouped_views[key] = value.uniq { |acc| acc.ip_address.value }
+      end
+
+      grouped_views
+    end
+
+    byebug
+  end
+  
+  private
+
+  def accumulate
+    [].tap do |array|
+      CSV.foreach(@file) do |row|
+        array << View.new(row.first)
+      end
+    end
   end
 end
--- spec/lib/commander_spec.rb
+++ spec/lib/commander_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
+
+
 RSpec.describe Commander, type: :service do
+  let(:file_path) { '../spec/support/webserver.log' }
+
   describe '#new' do
     subject(:do_initialize) { described_class.new(file_path: file_path) }
 
     context 'when the filepath is existing' do
-      let!(:file_path) { '../spec/support/webserver.log' }
-
       it 'returns an instance of the class and doesn\'t return the error' do
         expect(do_initialize).to be_instance_of described_class
       end
@@ -20,6 +22,28 @@ RSpec.describe Commander, type: :service do
           do_initialize
         }.to raise_error(Commander::InvalidFilePathError)
       end
+    end
+  end
+
+  describe '#unique_views' do
+    subject(:do_unique_views) { described_class.new(file_path: file_path).unique_views }
+
+    it 'compiles all of the unique views by ip_address' do
+      do_unique_views
+    end
+  end
+
+  describe '#accumulate' do
+    subject(:do_accumulate) { described_class.new(file_path: file_path).send(:accumulate) }
+
+    it 'accumulates the views for the file into objects' do
+      views = do_accumulate
+
+      expect(views).to be_instance_of Array
+      expect(views.length).to eq 500
+      expect(views.first).to be_instance_of View
+      expect(views.first.uri.value).to eq '/help_page/1'
+      expect(views.first.ip_address.value).to eq '126.318.035.038'
     end
   end
 end
